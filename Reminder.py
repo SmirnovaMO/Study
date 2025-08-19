9f3e2e8ef2d9be99ca7208928eeff19e56870d5a
892639o8773-lgtm <892639o8773@gmail.com>
2025-08-20 01:25:38 +0300

    Создаём главное окно

--- Reminder.py
+++ Reminder.py
@@ -1,10 +1,15 @@
-print('Hello, world!')
+from tkinter import *
+from tkinter import messagebox as mb
+from tkinter import simpledialog as sd
+import datetime
+import time
+import pygame
 
-print('Hello, GitHub!')
+window = Tk()
+window.title(title='Напоминание')
+label = Label('Установите напоминание')
+label.pack(pady=10)
+set_button = Button(text='Установить напоминание', command=set)
+set_button.pack
 
-x = 5
-y = 20
-print(x + y)
-
-z = 10
-print(z * x)
+window.mainloop()
